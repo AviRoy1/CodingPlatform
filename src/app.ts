b4d0c52801fff4921b8859f2bf0582e2585e196c
avijit <avijitroy2944@gmail.com>
2023-07-12 16:18:01 +0530

    mongoDB setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/database';
+
 
 
 dotenv.config();
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
